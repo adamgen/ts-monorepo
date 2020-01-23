dbfaf4b41a6eb1e0edd6839bf2ad79b4b4b0ac52
Adam <adamgensh@gmail.com>
2020-01-24 00:23:32 +0200

    Add dummy usage of appolo client in angular

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,30 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'frontend';
 
-  constructor(private httpClient: HttpClient) {
+  constructor(
+    private httpClient: HttpClient,
+    private apollo: Apollo,
+  ) {
 
   }
 
   async ngOnInit() {
-    const result = await this.httpClient.get('api/').toPromise();
-    console.log(result);
+    const result = await this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            author(id: 124){
+              id
+              firstName
+              lastName
+            }
+          }
+        `,
+      })
+      .valueChanges
+      .subscribe(result => {
+        console.log(result);
+      });
+
   }
 }
--- packages/frontend/src/app/app.module.ts
+++ packages/frontend/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GraphQLModule } from './graphql.module';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @NgModule({
   declarations: [
@@ -15,8 +18,21 @@ import { GraphQLModule } from './graphql.module';
     AppRoutingModule,
     HttpClientModule,
     GraphQLModule,
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: 'api/graphql'
+        })
+      };
+    },
+    deps: [HttpLink]
+  }
   ],
   bootstrap: [AppComponent]
 })
