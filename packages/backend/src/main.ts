49f1a33251d914c4fb76966816af2f3fd12bdc20
Adam <adamgensh@gmail.com>
2020-01-23 23:42:55 +0200

    Fix BACKEND_PORT

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import '@vista-point/common/lib/dotenv';
+import { BACKEND_PORT } from '@vista-point/common/lib/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.BACKEND_PORT);
-  console.log(`listening on port ${process.env.BACKEND_PORT}`);
+  console.log(`listening on port ${BACKEND_PORT}`);
 }
 bootstrap();
